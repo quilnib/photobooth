fad92313235990c220a66e33ee9af6186d173004
quilnib <timinth@gmail.com>
2016-11-09 02:15:24 +0000

    ukpdated script file to be able to access DISPLAY with proper XAUTHORITY reference

--- scripts/run_photobooth.sh
+++ scripts/run_photobooth.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #cd /home/pi/git/TouchSelfie/scripts
 
-
+export XAUTHORITY=/home/pi/.Xauthority
 export DISPLAY=:0.0
 sleep 10s;
-python /home/pi/git/TouchSelfie/scripts/photobooth_gui.py
+cd /home/pi/git/TouchSelfie/scripts
+python photobooth_gui.py
+#python /home/pi/git/TouchSelfie/scripts/photobooth_gui.py
 
 exit 0
